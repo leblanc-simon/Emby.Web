d4a6e12f799528aa4b3a0a206d352b3d96d4c990
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-11-03 00:49:08 -0500

    update images

--- plugins/defaulttheme/backdrop.js
+++ plugins/defaulttheme/backdrop.js
@@ -6,18 +6,18 @@
 
     function setStaticBackdrop() {
 
-        //var path = Emby.PluginManager.mapPath('defaulttheme', 'css/images/blur' + getRandomInt(1, 6) + '.png');
-        var path = Emby.PluginManager.mapPath('defaulttheme', 'css/images/blur7.png');
+        var path = Emby.PluginManager.mapPath('defaulttheme', 'css/images/blur' + getRandomInt(1, 6) + '.png');
+        //var path = Emby.PluginManager.mapPath('defaulttheme', 'css/images/blur7.png');
         Emby.Backdrop.setBackdrop(path);
 
         setTimeout(function() {
-            document.querySelector('.themeContainer').classList.add('staticBackdrop');
+            //document.querySelector('.themeContainer').classList.add('staticBackdrop');
         }, 1000);
     }
 
     function setBackdrops(items) {
 
-        document.querySelector('.themeContainer').classList.remove('staticBackdrop');
+        //document.querySelector('.themeContainer').classList.remove('staticBackdrop');
         Emby.Backdrop.setBackdrops(items);
     }
 
