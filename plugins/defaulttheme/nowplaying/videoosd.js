7b849473fa964835bf0bbd27888f3e04190ae44c
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-12-11 01:09:36 -0500

    update audio osd

--- plugins/defaulttheme/nowplaying/videoosd.js
+++ plugins/defaulttheme/nowplaying/videoosd.js
@@ -132,7 +132,9 @@
                   { transform: 'translate3d(0,' + elem.offsetHeight + 'px,0)', opacity: '.3', offset: 0 },
                   { transform: 'translate3d(0,0,0)', opacity: '1', offset: 1 }];
                 var timing = { duration: 300, iterations: 1, easing: 'ease-out' };
-                elem.animate(keyframes, timing);
+                elem.animate(keyframes, timing).onfinish = function() {
+                    Emby.FocusManager.autoFocus(elem, true);
+                };
             });
         }
 
